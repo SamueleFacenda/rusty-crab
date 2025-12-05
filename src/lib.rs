e51a398206db22960c39bc91a16169dcaf9934af
NeoEcc <149087244+NeoEcc@users.noreply.github.com>
2025-12-05 18:30:05 +0100

    Added functions to be implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use common_game::components::planet::{PlanetAI, PlanetState};
+use common_game::components::rocket::Rocket;
+use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
+use common_game::protocols as protocols;
+
+pub struct RustyCrabPlanet{
+    //TODO!
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl RustyCrabPlanet{
+    pub fn new() -> RustyCrabPlanet{
+        RustyCrabPlanet{}
     }
+}
+impl PlanetAI for RustyCrabPlanet{
+    fn handle_orchestrator_msg(&mut self, state: &mut PlanetState, msg: OrchestratorToPlanet) -> Option<PlanetToOrchestrator> {
+        todo!()
+    }
+
+    fn handle_explorer_msg(&mut self, state: &mut PlanetState, msg: ExplorerToPlanet) -> Option<PlanetToExplorer> {
+        todo!()
+    }
+
+    fn handle_asteroid(&mut self, state: &mut PlanetState) -> Option<Rocket> {
+        todo!()
+    }
+
+    fn start(&mut self, state: &PlanetState) {
+        todo!()
+    }
+
+    fn stop(&mut self) {
+        todo!()
+    }
+}
+
+pub fn create_planet(/*...*/) -> RustyCrabPlanet {
+    todo!()
 }
