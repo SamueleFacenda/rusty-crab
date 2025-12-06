9b15b49f57f2bd400d44fc57ad56926fbd301796
NeoEcc <149087244+NeoEcc@users.noreply.github.com>
2025-12-06 16:55:44 +0100

    Implemented sunray handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
-use common_game::components::planet::{Planet, PlanetAI, PlanetState, PlanetType};
+use common_game::components::planet::{Planet, PlanetAI, PlanetState};
 use common_game::components::rocket::Rocket;
 use common_game::protocols::messages;
 use std::sync::mpsc;
+use common_game::components::resource::{Combinator, Generator};
+use common_game::protocols::messages::PlanetToOrchestrator::SunrayAck;
 
 pub struct RustyCrabPlanetAI{ // Alternatively can be named ust "AI" as in the docs
     //TODO!
@@ -13,16 +15,77 @@ impl RustyCrabPlanetAI{
         RustyCrabPlanetAI{}
     }
 }
+#[allow(unused)]
 impl PlanetAI for RustyCrabPlanetAI{
-    fn handle_orchestrator_msg(&mut self, state: &mut PlanetState, msg: OrchestratorToPlanet) -> Option<PlanetToOrchestrator> {
+    fn handle_orchestrator_msg(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+        msg: OrchestratorToPlanet,
+    ) -> Option<PlanetToOrchestrator> {
+        match msg {
+            OrchestratorToPlanet::Sunray(sunray) => {
+                // If there is no energy cell, recharge one. Then,
+                // If there is no rocket, build one
+                // Else, do nothing
+
+                // Charge energy cell if it is empty
+                match state.empty_cell() {
+                    None => {
+                        // All cells are charged
+                    }
+                    Some((cell, _)) => {
+                        cell.charge(sunray);
+                    }
+                } // This is kind of superfluous since charging an already charged cell does nothing
+
+                // Create rocket if there is none
+                if !state.has_rocket() {
+                    if let Some((_, index)) = state.full_cell() {
+                        // To be moved to test
+                        assert!(index <= state.cells_count());
+                        assert_eq!(state.has_rocket(), false);
+                        // We have a charged cell, try to build a rocket
+                        let _ = state.build_rocket(index);
+                    }
+                }
+                
+                Some(SunrayAck { planet_id: state.id() })
+            }
+            OrchestratorToPlanet::Asteroid(_asteroid) => {
+                // If there is a rocket, fire it and attempt to rebuild it
+                // If there is no rocket, attempt to make one and fire it
+                // Else, die
+
+
+                None
+
+            }
+
+            OrchestratorToPlanet::StartPlanetAI => {
+                None
+            }
+            OrchestratorToPlanet::StopPlanetAI => {
+                None
+            }
+            OrchestratorToPlanet::InternalStateRequest => {
+                None
+            }
+            OrchestratorToPlanet::IncomingExplorerRequest { .. } => {
+                None
+            }
+            OrchestratorToPlanet::OutgoingExplorerRequest { .. } => {
+                None
+            }
+        }
+    }
+
+    fn handle_explorer_msg(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator, msg: ExplorerToPlanet) -> Option<PlanetToExplorer> {
         todo!()
     }
 
-    fn handle_explorer_msg(&mut self, state: &mut PlanetState, msg: ExplorerToPlanet) -> Option<PlanetToExplorer> {
-        todo!()
-    }
-
-    fn handle_asteroid(&mut self, state: &mut PlanetState) -> Option<Rocket> {
+    fn handle_asteroid(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator) -> Option<Rocket> {
         todo!()
     }
 
@@ -30,15 +93,16 @@ impl PlanetAI for RustyCrabPlanetAI{
         todo!()
     }
 
-    fn stop(&mut self) {
+    fn stop(&mut self, state: &PlanetState) {
         todo!()
     }
 }
 
+#[allow(unused)]
 pub fn create_planet(
     rx_orchestrator: mpsc::Receiver<messages::OrchestratorToPlanet>,
     tx_orchestrator: mpsc::Sender<messages::PlanetToOrchestrator>,
     rx_explorer: mpsc::Receiver<messages::ExplorerToPlanet>,
-) -> Planet<RustyCrabPlanetAI> {
+) -> Planet {
     todo!()
 }
