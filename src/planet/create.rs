3be0c7c37cf28b30cdf6818eb610f22dde05c761
SamueleFacenda <samuele.facenda@gmail.com>
2025-12-06 19:52:18 +0100

    Setup module and directory structure
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

---
+++ src/planet/create.rs
@@ -0,0 +1,29 @@
+use common_game::components::planet::{Planet, PlanetType};
+use common_game::protocols::messages;
+use common_game::components::resource::BasicResourceType::Carbon;
+use common_game::components::resource::ComplexResourceType::{AIPartner, Diamond, Dolphin, Life, Robot, Water};
+
+use super::ai::RustyCrabPlanetAI;
+
+#[allow(unused)]
+pub fn create_planet(
+    rx_orchestrator: crossbeam_channel::Receiver<messages::OrchestratorToPlanet>,
+    tx_orchestrator: crossbeam_channel::Sender<messages::PlanetToOrchestrator>,
+    rx_explorer: crossbeam_channel::Receiver<messages::ExplorerToPlanet>,
+) -> Planet {
+    let id = 96;
+    let ai = RustyCrabPlanetAI {};
+    let gen_rules = vec![Carbon];  // todo: choose which one (max. 1)
+    let comb_rules = vec![Diamond, Water, Life, Robot, Dolphin, AIPartner];
+
+    // Construct the planet and return it
+    Planet::new(
+        id,
+        PlanetType::C,
+        Box::new(ai),
+        gen_rules,
+        comb_rules,
+        (rx_orchestrator, tx_orchestrator),
+        rx_explorer,
+    ).expect("Failed to create the planet")
+}
