87458ae5a8f39971d1a0ca53b13ffbf72581b4fe
SamueleFacenda <samuele.facenda@gmail.com>
2025-12-06 19:12:18 +0100

    Add nix support
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Rusty Crab advanced programming project
+
+## Environment setup
+You can use [nix](https://nixos.org/explore/) to setup a developement environment, better with direnv.
+Just run `nix develop` to enter a shell with all the requirements available, or `nix build` to build
+the project without need to install anything else and pollute your sustem.
+If rust-rover doesn't find the std just run `echo $RUST_SRC_PATH` and use this path when it asks to attach the
+std manually.
---
+++ flake.nix
@@ -0,0 +1,59 @@
+# nix comments
+{
+  description = "Advanced programming 2025 library, RustyCrab";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.11";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      version = "0.1.0";
+      overlay = final: prev: { };
+    in
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = (nixpkgs.legacyPackages.${system}.extend overlay); 
+        rust-toolchain = pkgs.symlinkJoin {
+          name = "rust-toolchain";
+          paths = with pkgs; [ rustc cargo rustPlatform.rustcSrc ];
+        };
+      in
+      {
+
+        packages = rec {
+          default = rustycrab;
+          rustycrab = pkgs.rustPlatform.buildRustPackage {
+            pname = "rustycrab";
+            src = pkgs.lib.cleanSource ./.;
+            inherit version;
+
+            buildInputs = with pkgs; [ ];
+            
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
+            };
+          };
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            packages = with pkgs; [
+              rust-toolchain 
+              evcxr 
+              rustfmt
+              clippy
+              (python3.withPackages (ps: with ps; [
+
+              ]))
+            ];
+            RUST_BACKTRACE = 1;
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+        };
+      }
+    );
+}
