03098dff9eb67d9955881d60f502ad91ee1363c9
SamueleFacenda <samuele.facenda@gmail.com>
2025-12-07 08:54:00 +0100

    Implement logging and global config
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,7 @@ pub(crate) mod orchestrator;
 pub(crate) mod app;
 
 fn main() {
+    app::AppConfig::init();
+    app::setup_logger().expect("Failed to initialize logger");
     todo!("Run the orchestrator")
 }
