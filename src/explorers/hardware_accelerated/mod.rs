82fb1a44c7b830abffedfe46a721e75a782ac940
SamueleFacenda <samuele.facenda@gmail.com>
2026-01-30 21:55:16 +0100

    Write channels wrapper for logging
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- src/explorers/hardware_accelerated/mod.rs
+++ src/explorers/hardware_accelerated/mod.rs
@@ -1,3 +1,7 @@
 mod explorer;
+mod logging_channel;
+
+pub(self) use logging_channel::{OrchestratorLoggingReceiver, OrchestratorLoggingSender, PlanetLoggingReceiver, PlanetLoggingSender};
+
 
 pub(crate) use explorer::HardwareAcceleratedExplorer;
