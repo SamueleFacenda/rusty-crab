f851f680eb88fe12fbb4b105938f7f0d0feb5d33
SamueleFacenda <samuele.facenda@gmail.com>
2026-01-30 19:16:46 +0100

    Add definition for hardware accelerated explorer
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- src/explorers/mod.rs
+++ src/explorers/mod.rs
@@ -3,10 +3,12 @@
 
 mod example;
 mod explorer;
+mod hardware_accelerated;
 
 pub(crate) use explorer::{BagContent, Explorer, ExplorerBuilder};
 
 pub(crate) type ExampleExplorerBuilder = explorer::ExplorerBuilderImpl<example::ExampleExplorer>;
+pub(crate) type HardwareAcceleratedExplorerBuilder = explorer::ExplorerBuilderImpl<hardware_accelerated::HardwareAcceleratedExplorer>;
 
 pub(crate) struct ExplorerFactory;
 
@@ -14,6 +16,7 @@ impl ExplorerFactory {
     pub fn make_from_name(type_name: &String) -> Box<dyn ExplorerBuilder> {
         match type_name.to_ascii_lowercase().as_str() {
             "example" => Box::new(ExampleExplorerBuilder::new()),
+            "hardware_accelerated" => Box::new(HardwareAcceleratedExplorerBuilder::new()),
             _ => {
                 log::warn!(
                     "Explorer type '{type_name}' not recognized. Defaulting to 'example' explorer."
