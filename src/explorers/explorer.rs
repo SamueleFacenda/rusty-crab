c11b26a58e2f8c1496faf2bb49f26ebe22ecbb04
NeoEcc <149087244+NeoEcc@users.noreply.github.com>
2026-02-02 15:57:57 +0100

    Added meaningful value to bagcontent

--- src/explorers/explorer.rs
+++ src/explorers/explorer.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+use common_game::components::resource::ResourceType;
 use common_game::protocols::orchestrator_explorer::{ExplorerToOrchestrator, OrchestratorToExplorer};
 use common_game::protocols::planet_explorer::{ExplorerToPlanet, PlanetToExplorer};
 
 #[derive(Debug)]
-pub struct BagContent;
+pub struct BagContent{
+    content: HashMap<ResourceType, usize>
+}
 
 /// Trait defining the behavior of an Explorer,
 pub trait Explorer {
